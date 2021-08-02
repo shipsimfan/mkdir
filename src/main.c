ad985656a904ca5cedb62fc1909e56fdd9e7ae33
Lance Hart <shipsimfan@hotmail.com>
2021-08-02 12:47:56 -0400

    Implemented

--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
-int main() {
+#include <los/filesystem.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char* argv[]) {
+	if(argc != 2) {
+		printf("Usage: %s directory_name\n", argv[0]);
+		return 1;
+	}
+
+	int64_t status = create_directory(argv[1]);
+	if(status < 0) {
+		printf("Error while creating %s: %s\n", argv[1], strerror(status));
+		return 1;
+	}
+
 	return 0;
 }
